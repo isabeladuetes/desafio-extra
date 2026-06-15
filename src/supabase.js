ae29e3bcf2ba5b3a52a1b103e1b4bfb1c4cfd2df
Isabela Duetes <isabela.duetes@aluno.senai.br>
2026-06-15 09:45:12 -0300

    feat: organiza supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hhdjxdstfgqpftekveha.supabase.co';
-const supabaseAnonKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhoZGp4ZHN0ZmdxcGZ0ZWt2ZWhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1MTExMTcsImV4cCI6MjA5NzA4NzExN30.tJ8WVik1N6MgJUKkDF5qaSx0Q2LjK-H2XnpLd-vNz_c';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
